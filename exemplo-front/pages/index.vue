74a90a83bb5e57dda50bf3cdb2735e4438689040
unknown <mp.tiago@gmail.com>
2022-05-02 16:39:23 -0300

    Criação do exemplo Nuxt.js

---
+++ exemplo-front/pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div> 
+    <h1>Nuxt.js bem vindo!</h1>
+    <ul>
+      <li><a href="lista_pessoas"> Lista de pessoas </a></li>
+      <li>Item -2</li>
+      <li>Item -3</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
---
+++ exemplo-front/pages/lista_pessoas.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+      <h1> {{titulo}}</h1>
+      <ul>
+          <!-- Busca por items em pessoas e usa os dados-->
+          <li :key="item.id" v-for="item in pessoas">
+              <!-- Saída: Nome - Idade: X anos-->
+              {{item.nome}} - Idade: {{item.idade}} anos
+          </li>
+          <button @click="digaOla">  Clique aqui </button>
+      </ul>
+  </div>
+</template>
+
+<script>
+export default {
+    data: function(){
+        return{
+            titulo: "Lista de Pessoas e Idades",
+            pessoas: [
+                { id: 1, nome: "Marcio", idade: "33"},
+                { id: 2, nome: "Diego", idade: "44"},
+                { id: 3, nome: "João", idade: "50"},
+                { id: 4, nome: "Carla", idade: "24"},
+                { id: 5, nome: "José", idade: "28"},
+                { id: 6, nome: "Maria", idade: "22"},
+                { id: 7, nome: "Eduardo", idade: "29"},
+                { id: 8, nome: "Clara", idade: "26"},
+                { id: 9, nome: "Olivia", idade: "23"},
+            ]
+        }
+    },
+    methods:{
+        digaOla: function(){
+            alert("Isso é um alerta!");
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
